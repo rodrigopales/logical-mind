c25c0e295ea03532d0977e6d525f38999dde73d0
Rodrigo Pales <rtpales@gmail.com>
2024-11-19 07:48:34 -0300

    Day 2 variables and others

---
+++ dia2.py
@@ -0,0 +1,45 @@
+#Variables in python not typed
+idade = 34
+nome = "Rodrigo"
+altura = 1.68
+
+#Data type
+#int, float, textos = strings, bool = true ou false
+cidade = 'Anápolis'
+esta_logado = True
+nome = 2
+print(nome)
+
+#Check data type
+print(type(nome))
+print(type(cidade))
+print(type(esta_logado))
+print(type(altura))
+
+#Operators
+print(5 + 1.2 )
+print( 10 * 2)
+print(50 / 5 )
+print(50 - 25)
+print( 15.2 % 5)
+print( 2 ** 2)
+print(-3 + 12)
+#Concatenation
+nomeUsuario = " Mister Chap"
+frase = "Olá" + nomeUsuario + " tudo bem?"
+print(frase)
+
+#Comparisons
+maior = 15 > 2
+print(maior)
+
+menor = 15 < 12
+print(menor)
+
+print(5 == 5)
+
+#Python compares lowercase and uppercase
+print( "teste" == "teste")
+print( "Rodrigo" == "rodrigo")
+
+print ( 15!= 2)
