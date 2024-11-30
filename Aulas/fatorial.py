ae111de4dfcb9f38155483a5b06c70e0dc5f5c79
Rodrigo Pales <rtpales@gmail.com>
2024-11-30 12:20:15 -0300

    Remember Factorial

--- Aulas/fatorial.py
+++ Aulas/fatorial.py
@@ -16,3 +16,17 @@ def fatorial(n):
     return n * fatorial(n-1)
 resultado = fatorial(5)
 print(resultado)  # Imprime 120
+
+
+def fatorial_for(n):
+  """Calcula o fatorial usando um loop for."""
+  fatorial = 1
+  for i in range(1, n+1):
+    fatorial *= i
+  return fatorial
+
+import math
+
+def fatorial_math(n):
+  """Calcula o fatorial usando a função factorial da biblioteca math."""
+  return math.factorial(n)
