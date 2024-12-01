f87f5fe8bb535b9bb01904af5e098c449dcbd9d2
Rodrigo Pales <rtpales@gmail.com>
2024-12-01 15:33:52 -0300

    Factorial e Fibonnaci

--- Aulas/fatorial.py
+++ Aulas/fatorial.py
@@ -30,3 +30,14 @@ import math
 def fatorial_math(n):
   """Calcula o fatorial usando a função factorial da biblioteca math."""
   return math.factorial(n)
+
+N = int(input("Digite um número inteiro positivo: "))
+fatorial = 1
+if N < 0:
+  print("Não existe fatorial de número negativo.")
+elif N == 0 or N == 1:
+ print(f"O fatorial de {N} é 1.")
+else:
+ for i in range(1, N+1):
+  fatorial *= i
+ print(f"O fatorial de {N} é {fatorial}.")
