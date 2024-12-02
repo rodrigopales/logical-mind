6b39a2eb3f664b4c5a5d1348950e78d8f36b8fa1
Rodrigo Pales <rtpales@gmail.com>
2024-12-02 08:08:39 -0300

    Array and Tuple

---
+++ Aulas/dia6.py
@@ -0,0 +1,10 @@
+#Lista de (array) e tuplas
+#Interligados com os loops - estrutura de repetição
+
+marcaCaminhoes = ['Volvo','Mercedes Benz','Iveco','Man']
+
+#Os indices começam a contar de 0
+print(marcaCaminhoes[0])
+print(marcaCaminhoes[1])
+print(marcaCaminhoes[2])
+print(marcaCaminhoes[3])
