e49614f316694a586134427aba28ac5409b06aa6
Rodrigo Pales <rtpales@gmail.com>
2024-12-12 18:29:43 -0300

    Day 9 , String manipulation

---
+++ Aulas/dia9.py
@@ -0,0 +1,24 @@
+# Manipulação de Strings
+
+# Concatenação = juntar uma ou mais strings usando o operador +
+
+nome = "Alice"
+sobrenome = "Pales"
+print(nome + " " + sobrenome)
+
+# Repetição = multiplicar uma string por um número para repetir seus caracteres
+
+asteriscos = "*" * 10
+print(asteriscos) 
+
+# Indexação = acessar caracteres individuais de uma string utilizando colchetes. O primeiro caractere tem o índice 0
+
+palavra = "Python"
+primeiraLetra = palavra[0]
+print(primeiraLetra)
+
+# Fatiamento = extrair partes de uma string utilizando colchetes com dois índices separados por dois pontos
+
+frase = "Olá,mundo!"
+subfrase = frase[4:9]
+print(subfrase)
