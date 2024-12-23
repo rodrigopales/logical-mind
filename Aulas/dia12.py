a6491fd04cbf2e8bcbfad9d01af15d0bbba08fbc
Rodrigo Pales <rtpales@gmail.com>
2024-12-23 13:10:07 -0300

    Day 12 finish

--- Aulas/dia12.py
+++ Aulas/dia12.py
@@ -8,8 +8,10 @@
 # else => para uma condição positiva
 # finally => que roda sempre, independente da situação
 
+# outras linguagens => except = catch
+
 try:
-    arquivo = open("arquivso.txt", "r")
+    arquivo = open("arquivo.txt", "r")
     conteudo = arquivo.read()
 except FileNotFoundError:
     print("Erro: O arquivo 'arquivo.txt' não foi encontrado.")
@@ -18,3 +20,31 @@ else:
     print(conteudo)
 finally:
     print("Operação de leitura de arquivo finalizada.")
+    # Liberar memória
+    if 'arquivo' in locals():
+        arquivo.close()
+        print("Arquivo fechado!")
+
+# Tratamento de erro para o usuário 
+try:
+    numero = int(input("Digite um numero: "))
+    resultado = 100 / numero
+except ValueError:
+    print("Entrada inválida, tente novamente")
+except ZeroDivisionError:
+    print("Divisão por 0!")
+else:
+    print(f"O resultado é:{resultado}")
+finally:
+    print("Operação finalizada")
+
+# Tratamento de erro para o programador simplificando o código e usando erros do python mesmo para debugar
+try:
+    numero = int(input("Digite um numero: "))
+    resultado = 100 / numero
+except (ValueError,ZeroDivisionError) as error:
+    print("Erro: ", error)
+else:
+    print(f"O resultado é:{resultado}")
+finally:
+    print("Operação finalizada")
